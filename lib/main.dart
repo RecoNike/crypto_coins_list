5f696469ccbeecce7726e7b529651fb7393d8f0c
reconike <reconike@gmail.com>
2024-04-02 02:40:05 +0300

    rebuilded layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
+        primarySwatch: Colors.amber,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 29, 29, 29),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20)
+        ),
+        dividerColor: const Color.fromARGB(131, 255, 255, 255),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),
+          bodySmall: TextStyle(
+            color: Color.fromARGB(139, 255, 255, 255),
+            fontWeight: FontWeight.w700,
+            fontSize: 14
+          )
+        )
       ),
       home: const MyHomePage(title: 'Crypto currencies list'),
     );
@@ -41,31 +59,35 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 29, 29, 29),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.separated(
+        itemCount: 10,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, index) => ListTile(
+          leading: SvgPicture.asset(
+            'assets/svg/btc_logo.svg',
+             height: 25, width: 25
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          title: Text(
+            'Bitcoin',
+            style: theme.textTheme.bodyMedium
             ),
-          ],
+          subtitle: Text(
+            "0000",
+            style: theme.textTheme.bodySmall,
+            ),
+            trailing: const Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.white,
+              ),
+        )
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
