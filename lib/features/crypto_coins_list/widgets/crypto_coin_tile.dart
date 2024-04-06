2fed66c8a9e4a766d17e7e9102276c11c726f740
reconike <reconike@gmail.com>
2024-04-06 13:40:19 +0300

    minor

--- lib/features/crypto_coins_list/widgets/crypto_coin_tile.dart
+++ lib/features/crypto_coins_list/widgets/crypto_coin_tile.dart
@@ -9,29 +9,24 @@ class CryptoCoinTile extends StatelessWidget {
   });
 
   final CryptoCoin coin;
-  
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return ListTile(
       leading: Image.network(coin.imageUrl),
-      title: Text(
-        coin.name,
-        style: theme.textTheme.bodyMedium
-        ),
+      title: Text(coin.name, style: theme.textTheme.bodyMedium),
       subtitle: Text(
         "${coin.priceInUSD} \$",
         style: theme.textTheme.bodySmall,
-        ),
-        trailing: const Icon(
-          Icons.arrow_forward_ios,
-          color: Colors.white,
-          ),
-          onTap: (){
-            Navigator.of(context).pushNamed('/coin',
-            arguments: coin);
-          },
+      ),
+      trailing: const Icon(
+        Icons.arrow_forward_ios,
+        color: Colors.white,
+      ),
+      onTap: () {
+        Navigator.of(context).pushNamed('/coin', arguments: coin);
+      },
     );
   }
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 
 final mainTheme = ThemeData(
-        primarySwatch: Colors.deepPurple,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color.fromARGB(255, 29, 29, 29),
-          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
-          elevation: 0
+    primarySwatch: Colors.deepPurple,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+    useMaterial3: true,
+    appBarTheme: const AppBarTheme(
+        backgroundColor: Color.fromARGB(255, 29, 29, 29),
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+        elevation: 0),
+    dividerColor: const Color.fromARGB(131, 255, 255, 255),
+    textTheme: const TextTheme(
+        bodyMedium: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
         ),
-        dividerColor: const Color.fromARGB(131, 255, 255, 255),
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
-            fontSize: 20,
-          ),
-          bodySmall: TextStyle(
+        bodySmall: TextStyle(
             color: Color.fromARGB(139, 255, 255, 255),
             fontWeight: FontWeight.w700,
-            fontSize: 14
-          )
-        )
-      );
+            fontSize: 14)));
